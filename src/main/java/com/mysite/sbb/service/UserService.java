195c44f76df4a247ccb7c20595d499f64a1b3962
Sangwon Kim <66877863+lanasgit@users.noreply.github.com>
2022-11-02 18:30:06 +0900

    User 추가

---
+++ src/main/java/com/mysite/sbb/service/UserService.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.service;
+
+import com.mysite.sbb.domain.user.User;
+import com.mysite.sbb.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User create(String username, String email, String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+        return user;
+    }
+}
